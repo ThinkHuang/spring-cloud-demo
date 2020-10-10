aa34c88fa346456185be0efbed60dd2e64be7971
Third_Thinker <1070014845@qq.com>
2020-10-10 10:55:28 +0800

    添加consul服务发现实现，目前还存在问题，无法连接上服务

---
+++ spring-cloud-consul/src/main/java/org/spring/cloud/consul/SpringCloudClientConsulApplication.java
@@ -0,0 +1,14 @@
+package org.spring.cloud.consul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class SpringCloudClientConsulApplication 
+{
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudClientConsulApplication.class, args);
+    }
+}
