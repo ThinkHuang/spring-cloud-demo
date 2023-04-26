5e4c68c4e05c1dd209fcfedf1aeff98bc5779846
h10006547 <yejun.huang1@bthome.com>
2023-04-26 17:17:58 +0800

    重新构建项目

---
+++ spring-cloud-web/src/main/java/com/example/springcloudweb/SpringCloudWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcloudweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudWebApplication.class, args);
+    }
+
+}
